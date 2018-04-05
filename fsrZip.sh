5ed17e2eb01152b2bbb6f437a0d21d366c94d248
fernandoassisr <fernandoassissr@gmail.com>
2018-04-04 23:09:10 -0300

    Adicionar condição que verifica se existem diretorios a serem zipados

--- fsrZip.sh
+++ fsrZip.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-for i in */; do zip -r "${i%/}.zip" "$i";
-done
-echo "Arquivos Zipados com sucesso"
-echo '...'
+# Compacta todos os arquivos em um determinado diretório
+if [ "$(ls -d */ 2>/dev/null)" ];
+then
+    for i in */; 
+        do zip -r "${i%/}.zip" "$i";
+    done
+    echo "Arquivos Zipados com sucesso"
+    echo "..."
+else
+    echo "Nao ha diretorios a serem zipados"
+fi
