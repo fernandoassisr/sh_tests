4279ef00d79c3d60ae839e14ef371f45e87010cf
fernandoassisr <fernandoassissr@gmail.com>
2018-12-01 16:43:59 -0200

    Extrair o arquivo rar e compactar em zip

--- rar2zip.sh
+++ rar2zip.sh
@@ -7,6 +7,24 @@
 if hash unrar 2>/dev/null; then
 	# Body
 	atual=`pwd`
+	for arquivo in *.rar; do
+	   temp=/tmp/$arquivo
+	   if [ ! -e $temp ]; then
+    		mkdir $temp
+   	   fi
+	   
+	   unrar x $arquivo $temp
+
+	   cd $temp
+	  
+	   zip -r $atual/$arquivo.zip *
+
+	   cd $atual
+
+	   rm -fRd $temp
+
+	done
+
 else
 	echo "Necessario ter o unrar instalado para executar script"
 fi
