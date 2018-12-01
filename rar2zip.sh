68db2b3c0efad768d891a354f5adb447e02a6864
fernandoassisr <fernandoassissr@gmail.com>
2018-12-01 16:36:43 -0200

    Checar se unrar esta instalado

--- rar2zip.sh
+++ rar2zip.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
-# Descompacta arquivo rar para zip
+# Descompacta todos os arquivos rar de um diretorio para zip
 # Depedencias: unrar e zip
 
+# Verificar se o unrar está instalado
+
+if hash unrar 2>/dev/null; then
+	# Body
+	atual=`pwd`
+else
+	echo "Necessario ter o unrar instalado para executar script"
+fi
