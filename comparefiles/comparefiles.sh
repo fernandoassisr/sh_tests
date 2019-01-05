34854c27a26f2bc2ac90eae6ac05afcb5270be45
fernandoassisr <fernandoassissr@gmail.com>
2019-01-05 01:50:55 -0200

    Changing the output of the comparefiles

--- comparefiles/comparefiles.sh
+++ comparefiles/comparefiles.sh
@@ -1,4 +1,10 @@
 #! /bin/bash
 comm -2 -3 $1 $2 > result
-cat result
+if [ -s result ]
+then
+        echo 'Found some differences:    '
+		cat result
+else
+        echo 'There are no contents differences betwen those two files    '
+fi
 rm result
