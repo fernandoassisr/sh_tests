a02010e84603c39c76da2c1af44c96872169d26c
Fernando de Assis <fernandoassissr@gmail.com>
2019-01-08 16:18:14 -0200

    Add file description on comparefiles

--- comparefiles/comparefiles.sh
+++ comparefiles/comparefiles.sh
@@ -1,10 +1,12 @@
 #! /bin/bash
+# Using comm comand to find out the difference between two files content
+# extreamily limited
 comm -2 -3 $1 $2 > result
 if [ -s result ]
 then
         echo 'Found some differences:    '
 		cat result
 else
-        echo 'There are no contents differences betwen those two files    '
+        echo 'There are no contents differences between those two files    '
 fi
 rm result
