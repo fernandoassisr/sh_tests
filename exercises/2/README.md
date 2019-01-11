5551dbead344ce1ac9c28b4bb2827f291c672b73
Fernando de Assis <fernandoassissr@gmail.com>
2019-01-11 17:06:13 -0200

    Update the exercise 2 - finishing the task

--- exercises/2/README.md
+++ exercises/2/README.md
@@ -1,2 +1,2 @@
 # Exercise 2
-Read numbers from a external file, and search for some pattern. Put the result from your search on a result file
+Read some log from a external file, and search for a DATE pattern. Then put the result from your search on a result file.
--- exercises/2/searchPattern.sh
+++ exercises/2/searchPattern.sh
@@ -1,5 +1,9 @@
-#! /bin/bash/
+#! /bin/bash
+> result.data
 while read -r line
     do
-        declare "$line"
+        #declare "$line"
+        echo $line
+        echo $(echo $line | grep -oE '[a-zA-Z]{3} +[a-zA-Z]{3} +\w{1,2} +\w{2}:\w{2}:\w{2} +\w{4}') >> result.data
 	done < sample.data
+echo 'We found this number of matches: ' $(wc -l result.data)
